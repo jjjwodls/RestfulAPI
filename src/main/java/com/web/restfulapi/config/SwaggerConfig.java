04d189f86bfa712fd22e1df2e4cdb278e6d30d5f
JaessJang <jjjwodls@gmail.com>
2021-06-03 22:33:27 +0900

    스웨거 기본 설정 및 작업 진행

---
+++ src/main/java/com/web/restfulapi/config/SwaggerConfig.java
@@ -0,0 +1,33 @@
+package com.web.restfulapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    //api 관련된 Documentation 을 만든다.
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2);
+    }
+
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
+}
--- src/main/java/com/web/restfulapi/user/UserController.java
+++ src/main/java/com/web/restfulapi/user/UserController.java
@@ -1,5 +1,7 @@
 package com.web.restfulapi.user;
 
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -7,6 +9,9 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserController {
@@ -22,15 +27,27 @@ public class UserController {
         return service.findAll();
     }
 
+    /**
+     *
+     * @param id 조회할 id
+     * @return Hetaos를 통해 다른 사용자들이 추가 URL으로 다른 동작을 할 수 있는것을 정의함.
+     */
     @GetMapping("/users/{id}")
-    public User retriveUser(@PathVariable int id) {
+    public EntityModel<User> retriveUser(@PathVariable int id) {
         User user = service.findOne(id);
 
         if (user == null) {
             throw new UserNotFoundException(String.format("ID[%s] not found", id));
         }
 
-        return user;
+        //HATEOAS 작업 진행.
+        EntityModel<User> model = EntityModel.of(user); //현재 스프링부트 최신버전.
+        WebMvcLinkBuilder linkTo2 = linkTo(methodOn(this.getClass()).retriveAllUsers());
+
+        //해당 작업을 통해 all-users 조회에 대한 URL이 추가된다.
+        model.add(linkTo2.withRel("all-users"));
+
+        return model;
     }
 
     @PostMapping("/users")
